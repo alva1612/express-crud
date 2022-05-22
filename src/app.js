f4b28fb037b6f64430e23f4e8032ba8e91e00d84
alvaro <alvaro.guillenc1612@gmail.com>
2022-05-22 18:44:42 -0500

    22-05 HandlebarsSetUp

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 import express from "express";
 import indexRoutes from "./routes/index.routes";
 import ejs from "ejs";
+import {engine} from "express-handlebars";
 import path from "path";
 
 const app = express(); 
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'))
+//-----------------------------SET VIEWS
+app.set('views',path.join(__dirname,'views'));
 
+//----------------------------SET VIEW ENGINE
+//app.set('view engine','ejs');
+app.engine('.hbs', engine({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    defaultLayout: 'main',
+    extname: ".hbs",
+}));
+
+app.set('view engine', '.hbs');
+
+//----------------------------Rutas para la app
 app.use(indexRoutes);
-
-
 
 export default app;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,11 @@ import {Router} from "express"
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index");
+  });
+
+  router.get("/about", (req, res) => {
+    res.render("about");
   });
 
   export default router;
